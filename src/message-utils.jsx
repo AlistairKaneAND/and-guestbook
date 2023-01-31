d4318bfea31e9405056b15429143c6540179f0d9
AlistairKaneAND <alistair.kane@and.digital>
2023-01-31 12:31:56 +0000

    refactoring message utils

--- src/message-utils.jsx
+++ src/message-utils.jsx
@@ -2,7 +2,32 @@ import { useEffect, useReducer, useContext } from "react";
 import { fetchGuestbookEntries, postNewGuestbookEntry } from "./requests";
 import { createContext } from "react";
 
-export const MessageActionTypes = {
+export const useMessages = () => {
+  const [messages, dispatch] = useContext(MessageContext);
+
+  const addMessage = (payload) => {
+    return dispatch(addMessageAction(payload));
+  };
+
+  const updateGuestbookEntries = () => fetchEntries(dispatch);
+
+  return { messages, dispatch, updateGuestbookEntries, addMessage };
+};
+
+export const MessageContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(messageReducer, []);
+  useEffect(() => {
+    fetchEntries(dispatch);
+  }, []);
+
+  return (
+    <MessageContext.Provider value={[state, dispatch]}>
+      {children}
+    </MessageContext.Provider>
+  );
+};
+
+const MessageActionTypes = {
   ADD_MESSAGE: "ADD_MESSAGE",
   REQUEST_MESSAGES: "REQUEST_MESSAGES",
 };
@@ -25,18 +50,6 @@ const fetchEntries = async (dispatch) => {
   });
 };
 
-export const useMessages = () => {
-  const [messages, dispatch] = useContext(MessageContext);
-
-  const addMessage = (payload) => {
-    return dispatch(addMessageAction(payload));
-  };
-
-  const updateGuestbookEntries = () => fetchEntries(dispatch);
-
-  return { messages, dispatch, updateGuestbookEntries, addMessage };
-};
-
 const messageReducer = (prev, action) => {
   switch (action.type) {
     case MessageActionTypes.ADD_MESSAGE:
@@ -47,17 +60,4 @@ const messageReducer = (prev, action) => {
     default:
       throw new Error(`Unhandled action type ${action.type}`);
   }
-};
-
-export const MessageContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(messageReducer, []);
-  useEffect(() => {
-    fetchEntries(dispatch);
-  }, []);
-
-  return (
-    <MessageContext.Provider value={[state, dispatch]}>
-      {children}
-    </MessageContext.Provider>
-  );
 };
